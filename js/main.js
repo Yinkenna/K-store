c6345f59d96f8a4b8dfe3659455420f235cc5626
Yinkenna <khangnqdesign@gmail.com>
2023-02-28 11:37:47 +0700

    update new

--- js/main.js
+++ js/main.js
@@ -1,1 +1,5 @@
-console.log("hello world")
+console.log("Hi các bro, tui chỉ mới code giao diện thôi à không có chức năng gì hay ho đâu")
+console.log("Chờ trong tương lai đi nhá!!!")
+
+
+var html = document.getElementsByClassName
